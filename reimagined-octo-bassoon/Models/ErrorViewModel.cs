a473e57fed1510bb32d7aff0d2fc563183afeee2
Daniel Gustafsson <daniel.getset@gmail.com>
2020-03-29 21:20:25 +0200

    new mvc proj

---
+++ reimagined-octo-bassoon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reimagined_octo_bassoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
