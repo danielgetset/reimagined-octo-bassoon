699489178a68fb5387335264af3dab2d2408044f
Daniel Gustafsson <daniel.getset@gmail.com>
2020-03-29 22:01:50 +0200

    one component test

--- reimagined-octo-bassoon/src/main.js
+++ reimagined-octo-bassoon/src/main.js
@@ -1,9 +1,11 @@
-import Vue from 'vue'
+import Vue from "vue";
 //import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-window.Vue = Vue
+Vue.component("users-grid", require("./components/UsersGrid.vue").default);
+
+window.Vue = Vue;
 
 //new Vue({
 //  render: h => h(App),
